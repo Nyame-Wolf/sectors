8942e0016863a03449e7c3cc0bc0ebbd7c6a1a19
Nyame-Wolf <mumenyam@gmail.com>
2023-02-11 21:37:36 +0300

    add form reducer to the store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
-/* eslint-disable linebreak-style */
+/* eslint-disable */
 import { configureStore } from '@reduxjs/toolkit';
 import options from './reducers/options';
+import formPost from './reducers/formPost';
 
 const store = configureStore({
   reducer: {
     options,
+    formPost,
   },
 });
 

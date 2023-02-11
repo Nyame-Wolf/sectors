0ab8c3c37fa9ee064b9b07e75b97033662660e2a
Nyame-Wolf <mumenyam@gmail.com>
2023-02-11 21:38:23 +0300

    add form validation
    dispatch data from the store and to the db

--- src/components/SectorForm.js
+++ src/components/SectorForm.js
@@ -1,61 +1,144 @@
-/* eslint-disable  jsx-a11y/label-has-associated-control */
-import { useState } from 'react';
+/* eslint-disable  */
+import { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { postformData, postFormUpdate } from '../redux/reducers/formPost';
 
-import './SectorForm.css';
+import { getuserPreference } from '../redux/reducers/formPost';
+
 import Selector from './Selector';
+import './SectorForm.css';
 
 export default function SectorFrom() {
+  const preference = useSelector((state) => state.formPost);
   const [name, setName] = useState('');
+  const [nameTouched, setNameTouched] = useState(false);
   const [selectedOption, setSelectedOption] = useState('');
+  // const [selectedOptionId, setSelectedOptionId] = useState('');
   const [isChecked, setIsChecked] = useState(false);
+  const [error, setError] = useState('');
+  const [success, setSuccess] = useState(false);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getuserPreference());
+  }, []);
+
+  useEffect(() => {
+    setName(preference.name);
+    setSelectedOption(preference.option);
+    setIsChecked(preference.terms);
+  }, [preference]);
+
+  const nameIsValid = name.trim() !== '';
+  const nameInputIsInvalid = !nameIsValid && nameTouched;
 
   const handleNameChange = (event) => {
     setName(event.target.value);
   };
 
+  const nameInputBlurHandler = (event) => {
+    setNameTouched(true);
+  };
+
   const handleSelectChange = (event) => {
+    // setSelectedOptionId(event.target.selectedOptions[0].id);
     setSelectedOption(event.target.value);
   };
 
   const handleTermsChange = (event) => {
     setIsChecked(event.target.checked);
+
+    if (isChecked) {
+      setError('');
+    }
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
+    setNameTouched(true);
+    if (!nameIsValid || !isChecked) {
+      setError('Please agree to the terms to continue');
+      return;
+    }
+    const formData = {
+      name,
+      option: selectedOption,
+      terms: isChecked,
+    };
+
+    if (preference.id) {
+      dispatch(postFormUpdate(formData));
+    } else {
+      dispatch(postformData(formData));
+    }
+
+    setSuccess(true);
+
     setName('');
+    setNameTouched(false);
     setSelectedOption('Manufacturing');
     setIsChecked(false);
   };
+
+  const nameInputClasses = nameInputIsInvalid
+    ? 'form-control invalid'
+    : 'form-control';
+
   return (
     <form onSubmit={handleSubmit}>
-      <label htmlFor="name">
-        Name
-        <input type="text" id="name" value={name} onChange={handleNameChange} />
-      </label>
-      <label htmlFor="selectors">
-        Sectors:
-        <Selector
-          value={selectedOption}
-          onChange={handleSelectChange}
-          id="sectors"
-        />
-      </label>
-      <label htmlFor="terms">
-        Agree to terms
-        <input
-          type="checkbox"
-          id="terms"
-          checked={isChecked}
-          onChange={handleTermsChange}
-        />
-      </label>
+      <h3>
+        Please enter your name and pick the Sectors you are currently involved
+        in.
+      </h3>
+      <div className={nameInputClasses}>
+        <div>
+          <label htmlFor="name">
+            Name
+            <input
+              type="text"
+              id="name"
+              value={name || preference.name}
+              onChange={handleNameChange}
+              onBlur={nameInputBlurHandler}
+            />
+          </label>
+          {nameInputIsInvalid && (
+            <p className="error-text">Name can't be blank.</p>
+          )}
+        </div>
+        <label htmlFor="selectors">
+          Sectors:
+          <Selector
+            value={selectedOption}
+            onChange={handleSelectChange}
+            // sector_id={id}
+            id="sectors"
+          />
+        </label>
 
-      <label htmlFor="save">
-        Save
-        <input type="submit" id="save" />
-      </label>
+        <div>
+          <label htmlFor="terms">
+            Agree to terms
+            <input
+              type="checkbox"
+              id="terms"
+              checked={isChecked}
+              onChange={handleTermsChange}
+            />
+          </label>
+          {error && <p className="error-text">{error}</p>}
+        </div>
+      </div>
+      <div className="form-submit">
+        <button type="submit" id="save">
+          Save
+        </button>
+      </div>
+      {success && (
+        <p className="success">Your input has been successfully submitted</p>
+      )}
     </form>
   );
 }

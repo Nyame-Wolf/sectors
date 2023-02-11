87ba094b3288a83ebd0764712e8727f9d18473a7
Nyame-Wolf <mumenyam@gmail.com>
2023-02-11 21:36:51 +0300

    add reducers to post data data and update the data

---
+++ src/redux/reducers/formPost.js
@@ -0,0 +1,81 @@
+/* eslint-disable */
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+const initialState = {
+  name: '',
+  option: '',
+  terms: false,
+};
+
+export const postformData = createAsyncThunk(
+  'user/userPreference',
+  async (user_preference) => {
+    return await fetch('http://localhost:4000/user_preferences', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ user_preference }),
+    }).then(async (res) => {
+      if (res.ok) {
+        const response = await res.json();
+        localStorage.setItem('user_preference', response.id);
+        return response;
+      }
+      throw new Error(res);
+    });
+  }
+);
+
+export const postFormUpdate = createAsyncThunk(
+  'user/userPreferenceUpdate',
+  async (user_preference) => {
+    const id = localStorage.getItem('user_preference');
+    return await fetch(`http://localhost:4000/user_preferences/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ user_preference }),
+    }).then(async (res) => {
+      if (res.ok) {
+        const response = await res.json();
+        localStorage.setItem('user_preference', response.id);
+        return response;
+      }
+      throw new Error(res);
+    });
+  }
+);
+
+export const getuserPreference = createAsyncThunk(
+  'user/getUserPreference',
+  async () => {
+    const id = localStorage.getItem('user_preference');
+    return await fetch(`http://localhost:4000/user_preferences/${id}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }).then(async (res) => {
+      if (res.ok) {
+        const response = await res.json();
+        return response;
+      }
+      throw new Error(res);
+    });
+  }
+);
+
+export const formDataSlice = createSlice({
+  name: 'formData',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(postformData.fulfilled, (state, action) => action.payload)
+      .addCase(getuserPreference.fulfilled, (state, action) => action.payload);
+  },
+});
+
+export default formDataSlice.reducer;
